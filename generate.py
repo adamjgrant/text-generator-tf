c53c7656dbada92b7627c79519eee987289363ef
Adam Kochanowicz <ajkochanowicz@gmail.com>
2017-08-12 11:54:50 -0700

    Fixing dictionary assignments

--- generate.py
+++ generate.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-import numpy as np
+#import tensorflow as tf
+#import numpy as np
 
 # This will create two dictionaries, word->num and num->word
 # In order for the script to work with more efficient data.
@@ -7,7 +7,7 @@ def build_dataset(words):
   dictionary = dict()
   
   for word in words:
-    if not (bool(dictionary.get(word, False))):
+    if (dictionary.get(word) == None):
       print("Assigning {0} to dictionary whose length is {1}".format(word, len(dictionary)))
       dictionary[word] = len(dictionary)
     
